d0d5c2c10de34cbefe860e90f383f04f44fdd63b
Gqbo <gabriel.gonzaf@gmail.com>
2023-04-05 15:45:55 -0600

    Etapa 1: Se agrega documentación al test y se modulariza el código.

--- testFiguras.cc
+++ testFiguras.cc
@@ -11,11 +11,29 @@
 
 #define SIZE 1024
 
+/**
+ * @brief Muestra cuales figuras se encuentran disponibles en el servidor de piezas.
+ * @return std::string figures_string con las figuras disponibles.
+ */
+std::string showFigures();
+
+/**
+ * @brief Pide al cliente que digite la figura que desea
+ * @return std::string input_string con la figura solicitada por el cliente
+ */
+std::string requestFigure();
+
+/**
+ * @brief Extrae de la respuesta del servidor web la lista de piezas de Lego y la muestra en pantalla.
+ * @param html std::string con la respuesta del servidor web.
+ */
+void displayLegos(std::string html);
+
 int main( int argc, char * argv[] ) {
-   // Solicitar el nombre de la figura al usuario.
-   std::string input_string;
-   std::cout << "Ingrese el nombre de una figura: ";
-   std::getline(std::cin, input_string);
+   // Muestra las figuras que se pueden escoger
+   // showFigures();
+   // Solicitar el nombre de la figura al usuario y lo almacena en input_string.
+   std::string input_string = requestFigure();
 
    // Generar la solicitud HTTP.
    std::string url = "GET /lego/list.php?figure=" + input_string + " HTTP/1.1\r\nhost: redes.ecci\r\n\r\n";
@@ -26,24 +44,45 @@ int main( int argc, char * argv[] ) {
    char buffer[ SIZE ];
    int count;
    int itr_count = 0;  // Contador de iteraciones.
-   // String para almacenar la respuesta del servidor web.
-   std::string html;
 
    socket.InitSSL(); // Se inicializa el socket SSL.
    socket.SSLConnect( os, (char *) "https" ); // Se conecta el socket SSL al servidor web.
    socket.SSLWrite(  (void *) reqOS, strlen( reqOS ));
-   // Leer la respuesta del servidor web.
-   while (  (count = socket.SSLRead( buffer, SIZE )) > 0 ) {
-    
+
+   // String para almacenar la respuesta del servidor web.
+   std::string html;
+   while ( (count = socket.SSLRead( buffer, SIZE )) > 0 ) {
       if(itr_count >= 1) {
          buffer[count] = '\0'; // Se agrega un terminador nulo al final del string.
-         html += buffer;  // Se agrega el texto leido al string html
+         html += buffer;       // Se agrega el texto leido al string html
       }
       memset( buffer, 0, SIZE );
       itr_count++;
    }
+
+   displayLegos(html);
+
+   // Borrar el buffer de solicitud.
+   delete[] reqOS;
+   return 0;
+}
+
+std::string showFigures()  {
+   std::string figures_string;
+   return figures_string;
+}
+
+std::string requestFigure() {
+   // Solicitar el nombre de la figura al usuario.
+   std::string input_string;
+   std::cout << "Ingrese el nombre de una figura: ";
+   std::getline(std::cin, input_string);
+   return input_string;
+}
+
+void displayLegos(std::string html) {
    // Inicializar la cantidad total de piezas de Lego en 0.
-   int totalQuantity = 0;
+   int total_quantity = 0;
    // La expresión regular para coincidir con la lista de piezas de Lego.
    std::regex regex(R"(<TR><TD ALIGN=center>\s*(\d+)\s*</TD>\s*<TD ALIGN=center>\s*(.+?)\s*</TD>)");
 
@@ -56,7 +95,7 @@ int main( int argc, char * argv[] ) {
       std::smatch match = *it;
       // Extraiga la cantidad y descripción de la pieza de Lego.
       int quantity = std::stoi(match[1].str());
-      totalQuantity += quantity;
+      total_quantity += quantity;
       std::string description = match[2].str();
       // Generar el resultado
       std::cout << quantity << " " << description << std::endl;
@@ -64,13 +103,10 @@ int main( int argc, char * argv[] ) {
       ++it;
    }
    // Verificar si se encontraron piezas de Lego.
-   if (totalQuantity == 0) {
+   if (total_quantity == 0) {
       std::cout << "La figura no existe o no se encontraron piezas de lego para esta figura." << std::endl;
    } else {
       // Imprimir el total de piezas de Lego.
-      std::cout << "Total de piezas para armar esta figura: " << totalQuantity << std::endl;
+      std::cout << "Total de piezas para armar esta figura: " << total_quantity << std::endl;
    }
-   // Borrar el buffer de solicitud.
-   delete[] reqOS;
-   return 0;
 }
