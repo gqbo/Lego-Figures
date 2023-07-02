103ea9d9ad3d7e78d07cf40773e66ef5a8d6ca2c
Gabriel Gonzalez <gabriel.gonzaf@gmail.com>
2023-07-02 16:46:30 -0600

    Etapa 4: Se agrega header del protocolo del grupo.
    Co-authored-by: Juan Torres <JuanAguilarTorres@users.noreply.github.com>
    Co-authored-by: Sebastián Rodríguez T <sebastianrodrigueztencio@users.noreply.github.com>

---
+++ Etapa 4/ProtocolHeader.h
@@ -0,0 +1,47 @@
+#ifndef PROTOCOL_HEADER_HPP
+#define PROTOCOL_HEADER_HPP
+
+#include <errno.h>  // For Error Codes
+
+/*---------------------------------Ports--------------------------------------*/
+
+#define INTERMEDIARY_UDP_PORT 3141  /**< Intermediary Server UDP Port */
+#define INTERMEDIARY_TCP_PORT 3142  /**< Intermediary Server TCP Port */
+
+#define PIECES_UDP_PORT 4849  /**< Pieces Server UDP Port */
+#define PIECES_TCP_PORT 4850  /**< Pieces Server UDP Port */
+
+/*--------------------------------Timeouts------------------------------------*/
+
+#define FIRST_TIMEOUT 100  /**< First timeout: miliseconds */
+#define SECOND_TIMEOUT 200 /**< Second timeout: miliseconds*/
+#define THIRD_TIMEOUT 300  /**< Third timeout: miliseconds*/
+
+/*-----------------------------Separator char---------------------------------*/
+
+#define SEPARATOR char(29)  /**< Protocol message separator */
+
+/*-----------------------------Buffer-----------------------------*/
+
+#define BUFSIZE 512
+
+/*-----------------------Socket Connection Refused Error----------------------*/
+
+#define CONNECTION_REFUSED_ERROR ECONNREFUSED /**< From errno ECONNREFUSED */
+
+/*----------------------------Message code enum-------------------------------*/
+
+/**
+ * @brief Enum LegoMessageCode
+ * @details Defines the protocol types of messages
+ * 
+ */
+enum LegoMessageCode {
+  LEGO_DISCOVER,  /**< LEGO_DISCOVER type */
+  LEGO_PRESENT,   /**< LEGO_PRESENT type */
+  LEGO_REQUEST,   /**< LEGO_REQUEST type */
+  LEGO_RESPONSE,  /**< LEGO_RESPONSE type */
+  LEGO_RELEASE    /**< LEGO_RELEASE type */
+};  // LegoMessageCode
+
+#endif  // PROTOCOL_HEADER_HPP
