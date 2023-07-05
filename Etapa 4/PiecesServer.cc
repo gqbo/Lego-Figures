da845e81dc8713c3eb49b0693390a1c1af1794ea
Gabriel Gonzalez <gabriel.gonzaf@gmail.com>
2023-07-05 00:18:05 -0600

    Etapa 4: Se crea hilo UDP que va a esperar por mensajes hacia el servidor de piezas.
    Co-authored-by: Juan Torres <JuanAguilarTorres@users.noreply.github.com>
    Co-authored-by: Sebastián Rodríguez T <sebastianrodrigueztencio@users.noreply.github.com>

--- Etapa 4/PiecesServer.cc
+++ Etapa 4/PiecesServer.cc
@@ -1,28 +1,50 @@
 #include "MethodsPieces.h"
 
+void task( Socket * client ) {
+    MethodsPieces mp;
+    char buffer[1024];
+    struct sockaddr other;
+    char code_number;
+    memset( &other, 0, sizeof( other ) );
+    mp.sendPresent();
+    /*----------- RECEIVES UDP MESSAGES ------------*/
+    printf("Pieces (LOCAL): Esperando a recibir mensajes en el puerto PIECES_UDP_PORT...\n");
+    for ( ; ; ) {
+        client->recvFrom((void*)buffer, sizeof(buffer), (void*)&other);
+        code_number = buffer[0];
+        // switch
+        switch (code_number) {
+        case '0' /* HANDLES DISCOVER MESSAGE AND SENDS PRESENT MESSAGE */:
+        // Se tiene que utilizar la info recibida del broadcast, ahora lo hacemos sin utilizarlo
+            printf("Pieces (LOCAL): Mensaje BROADCAST recibido: %s\n", buffer);
+            mp.sendPresent(buffer);
+            break;
+        
+        default:
+            break;
+        }
+    }
+   
+}
+
 int main(int argc, char** argv) {
     MethodsPieces mp;
+    std::thread * workerUDP;
     char buffer[1024];
 
     /*----------- CREATES SOCKET UDP ------------*/
+    /*-------- BINDS TO PIECES_UDP_PORT ---------*/
     Socket* socketUDP;
     socketUDP = new Socket('d');
     struct sockaddr_in other;
     socketUDP->Bind(PIECES_UDP_PORT);
-    int enableBroadcast = 1;
-    setsockopt(socketUDP->getIDSocket(), SOL_SOCKET, SO_BROADCAST, &enableBroadcast, sizeof(enableBroadcast));
-    memset( &other, 0, sizeof( other ) );
-    printf("Pieces: Socket UDP bind a 127.0.0.1:PIECES_UDP_PORT\n");
+    printf("Pieces: Socket UDP bind a PIECES_UDP_PORT\n");
 
-    /*----------- RECEIVES UDP MESSAGES ------------*/
-    printf("Pieces (127.0.0.1): Esperando a recibir mensajes en el puerto PIECES_UDP_PORT...\n");
-    int n = socketUDP->recvFrom((void*)buffer, sizeof(buffer), (void*)&other);
-    buffer[n] = '\0'; 
-    printf("Pieces (127.0.0.1): Mensaje BROADCAST recibido: %s\n", buffer);
+    workerUDP = new std::thread( task, socketUDP ); // CREA SOLO 1 HILO
 
-    /*----------- HANDLES UDP MESSAGES ------------*/
-    // Se tiene que utilizar la info recibida del broadcast, ahora lo hacemos sin utilizarlo
-    mp.handlePresent();
+    // Espera a que el hilo termine
+    workerUDP->join();
+
 }
 
     // mi.addMapEntry("dalmata", "172.16.168.82");
@@ -39,3 +61,9 @@ int main(int argc, char** argv) {
     //     }
     //     std::cout << std::endl;
     // }
+
+    // /*----------- RECEIVES UDP MESSAGES ------------*/
+    // printf("Pieces (127.0.0.1): Esperando a recibir mensajes en el puerto PIECES_UDP_PORT...\n");
+    // int n = socketUDP->recvFrom((void*)buffer, sizeof(buffer), (void*)&other);
+    // buffer[n] = '\0'; 
+    // printf("Pieces (127.0.0.1): Mensaje BROADCAST recibido: %s\n", buffer);
